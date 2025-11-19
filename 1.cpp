1630340eac435f6ff89caa6dba86b87ddd1f1204
shashanksyt-debug <shashanksyt@gmail.com>
2025-11-19 09:49:50 +0530

    Master 1

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
 int main() {
+  string name;
   cout << "Hello World!!" << endl;
   cout << "Welcome to the project!!" << endl;
+  cout << "Enter your name: ";
+  cin >> name;
   return 0;
 }
